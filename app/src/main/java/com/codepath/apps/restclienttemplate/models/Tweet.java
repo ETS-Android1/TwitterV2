8cf9391c9228110a4f438c385d443ecaf04d6427
Adnan <adnank9465@gmail.com>
2021-10-03 00:54:08 -0700

    Added infinite scroll

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -14,6 +14,7 @@ public class Tweet {
     public String body;
     public String createdAt;
     public User user;
+    public long id;
 
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
@@ -21,6 +22,7 @@ public class Tweet {
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        tweet.id = jsonObject.getLong("id");
         return tweet;
 
     }
